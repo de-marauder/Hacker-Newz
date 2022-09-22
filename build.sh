16410f2686f52d3ac1a7d0565e8fd7445b0d84a3
de-marauder <ezikegodson@gmail.com>
2022-09-22 16:44:14 +0100

    App set for deployment on render

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 # exit on error
 set -o errexit
+pip3 install --upgrade pip
+pip3 install -r requirements.txt
 
-pip install -r requirements.txt
-
-python manage.py collectstatic --no-input
-python manage.py migrate
+python3 manage.py collectstatic --no-input
+python3 manage.py migrate

4a6917d997d504a72a5c75d08b5cfea6c72df7d6
de-marauder <ezikegodson@gmail.com>
2022-09-22 17:08:21 +0100

    App set for deployment on render

--- build.sh
+++ build.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 # exit on error
+uname -a
 set -o errexit
 curl -sSL https://bootstrap.pypa.io/get-pip.py | python3
 pip3 --version
+which python3
+python3 -m pip3 install --upgrade pip
 
 pip3 install -r requirements.txt
 

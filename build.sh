151baa5ac601f66083dc0d0a9de7da7848064ea9
de-marauder <ezikegodson@gmail.com>
2022-09-22 20:07:54 +0100

    deploying to render

--- build.sh
+++ build.sh
@@ -7,9 +7,15 @@ curl -sSL https://install.python-poetry.org | python3 -
 
 # Add poetry command to PATH
 export PATH=${PATH}:${HOME}/.local/bin
-
+echo '=================='
+echo ${PATH}
+echo '=================='
 poetry --version
+echo '=================='
+poetry shell
+echo '=================='
 poetry install
+echo '=================='
 
 # Gather static files and migrate database
 python3 manage.py collectstatic --no-input
